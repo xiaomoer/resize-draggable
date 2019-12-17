cfe1c2440b87f4d104c8969d72374f28ecf9d969
moyingtao <moyingtao@iwubida.com>
2019-12-17 13:18:59 +0800

    [*] update demo

--- demo/index.js
+++ demo/index.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Draggleable from '../dist/bundle';
+import '../dist/main.css';
 import './index.css';
 
 const App = () => (
   <div>
+    <h2>draggable and resizable, direction 'west'</h2>
     <div className="container-v">
       <div className="prev">Prev Element</div>
       <Draggleable direction="w" />
       <div className="next">Next Element</div>
     </div>
+    <h2>resizable, direction 'west'</h2>
     <div className="container-v">
       <div className="prev">Prev Element</div>
       <Draggleable direction="w" dragDisabled />
       <div className="next">Next Element</div>
     </div>
+    <h2>draggable and resizable, adaptive, direction 'east'</h2>
     <div className="container-v">
-      <div className="prev">Prev Element</div>
+      <div className="next">Prev Element</div>
       <Draggleable direction="e" adaptive />
-      <div className="next">Next Element</div>
+      <div className="prev">Next Element</div>
     </div>
+    <h2>draggable and resizable, direction 'north'</h2>
     <div className="container-h">
       <div className="prev-h">Prev Element</div>
       <Draggleable direction="n" />
       <div className="next-h">Next Element</div>
     </div>
+    <h2>draggable and resizable, direction 'south'</h2>
     <div className="container-h">
       <div className="prev-h">Prev Element</div>
       <Draggleable direction="s" />
